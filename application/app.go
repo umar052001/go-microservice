8d949561fc985d921247f7c5d87822b2497cb578
umar052001 <umar.111nawaz@gmail.com>
2024-12-27 02:02:46 +0500

    made connection to redis

--- application/app.go
+++ application/app.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -16,7 +17,11 @@ type App struct {
 func New() *App {
 	app := &App{
 		router: loadRoutes(),
-		rdb:    redis.NewClient(&redis.Options{}),
+		rdb: redis.NewClient(&redis.Options{
+			Addr:     "localhost:6379",
+			Password: "", // No password set
+			DB:       0,  // Default DB
+		}),
 	}
 
 	return app
@@ -24,7 +29,7 @@ func New() *App {
 
 func (a *App) Start(ctx context.Context) error {
 	server := &http.Server{
-		Addr:    ":3000",
+		Addr:    ":3001",
 		Handler: a.router,
 	}
 
@@ -32,7 +37,11 @@ func (a *App) Start(ctx context.Context) error {
 	if err != nil {
 		return fmt.Errorf("failed to connect to redis: %w", err)
 	}
-
+	defer func() {
+		if err := a.rdb.Close(); err != nil {
+			fmt.Println("failed to close redis", err)
+		}
+	}()
 	fmt.Println("Starting server")
 
 	ch := make(chan error, 1)
@@ -43,6 +52,16 @@ func (a *App) Start(ctx context.Context) error {
 		}
 		close(ch)
 	}()
+
+	select {
+	case err = <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
+		return server.Shutdown(timeout)
+	}
 
 	return nil
 }
